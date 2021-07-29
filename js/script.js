fa50b82519d7488341184dd5cbdf30d78fd009a6
unchnm <unchnm@yandex.ru>
2021-07-29 22:33:14 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/* const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMoviesDB = {
     count: numberOfFilms,
@@ -16,4 +16,13 @@ const a = prompt('Один из последних просмотренных ф
 
       personalMoviesDB.movies[a] = b;
 
-      console.log (personalMoviesDB);
+      console.log (personalMoviesDB); */
+
+      for (let i = 0; i<2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt ('На сколько оцените его?', ''); 
+           if (a == "" || a == null || a.length > 50 || b == "" || b == null || b.length > 50){
+            i--;
+           }     
+      }
+      
